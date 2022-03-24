57dd13e5b566e451ece814d6ac9f7eab52fe9a10
Jerome <josejeromelalunio@gmail.com>
2022-03-25 05:17:16 +0800

    support links for sauce finder

--- main.py
+++ main.py
@@ -34,48 +34,59 @@ async def on_ready():
 
 @ client.event
 async def on_message(message):
+    annie_id = "<@!955202644702556260>"
+
     if message.author == client.user:
         return
 
-    if "<@!955202644702556260>" in message.content:
+    if annie_id in message.content:
         if "hello" in message.content:
             await message.channel.send("hello there!")
             return
 
         if "sauce" in message.content:
+            sauce = None
             try:
                 if not message.attachments:
-                    await message.channel.send("Please include an anime screenshot.")
+                    link = None
+                    for word in message.content.replace(annie_id, "").split():
+                        if "http" in word:
+                            link = word
+                    if link:
+                        sauce = await get_sauce(link)
+                    else:
+                        await message.channel.send("Please include an anime screenshot or a link to a screenshot.")
                 else:
                     sauce = await get_sauce(message.attachments[0].url)
 
-                    embed = discord.Embed(
-                        title="✅ Sauce Found!",
-                        color=discord.Color.yellow()
-                    )
-                    embed.set_thumbnail(url=sauce["thumbnail"])
-                    embed.add_field(
-                        name="Title", value=sauce["title"] or "unknown", inline=True)
-                    embed.add_field(name="Accuracy",
-                                    value=f"{sauce['accuracy']}%", inline=True)
-                    embed.add_field(
-                        name="Source", value=sauce["link"] or "link not available", inline=False)
-
-                    await message.channel.send(embed=embed)
-
-                    if sauce['accuracy'] < 60:
-                        await message.channel.send(
-                            "Sorry I couldn't find good matches, are you sure this is an anime screenshot?"
-                        )
-
             except Exception as exception:
                 await message.channel.send(str(exception))
-        if "pic" in message.content:
-            print(message.attachments[0].url)
-            await message.channel.send("test")
-            return
 
-    elif "<@!955202644702556260>" in message.content:
+            if sauce:
+                embed = discord.Embed(
+                    title="✅ Sauce Found!",
+                    color=discord.Color.yellow()
+                )
+                embed.set_thumbnail(url=sauce["thumbnail"])
+                embed.add_field(
+                    name="Title", value=sauce["title"] or "unknown", inline=True)
+                embed.add_field(name="Accuracy",
+                                value=f"{sauce['accuracy']}%", inline=True)
+                embed.add_field(
+                    name="Source", value=sauce["link"] or "link not available", inline=False)
+
+                await message.channel.send(embed=embed)
+
+                if sauce['accuracy'] < 60:
+                    await message.channel.send(
+                        "Sorry I couldn't find good matches, are you sure this is an anime screenshot?"
+                    )
+
+        if "test" in message.content:
+            await message.channel.send("test")
+        return
+
+    elif annie_id in message.content:
         await message.channel.send("I have no Idea what you're talking about")
         return
 
